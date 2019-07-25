fb0e835ac6f3f784c1f4e78a546a852264f17a43
fshenton <fshenton@gmail.com>
2019-07-25 16:09:00 +0100

    Did some spring cleaning for all files to improve readibility and to remove unused code/comments/logs.

--- js/pokedex.js
+++ js/pokedex.js
@@ -19,15 +19,16 @@ class Pokemon {
 
 //TODO: likely will want to do some spring cleaning here, seems overly large
 const state = {
-	listButtons: undefined, //probs not needed?
-	pokemans: undefined,
-	image: undefined,
-	id: undefined,
-	name: undefined,
-	type: undefined,
-	weight: undefined,
-	height: undefined,
-	desc: undefined
+	//all values start undefined 
+	listButtons: 	undefined, 
+	pokemans: 		undefined,
+	image: 			undefined,
+	id: 			undefined,
+	name:			undefined,
+	type: 			undefined,
+	weight: 		undefined,
+	height: 		undefined,
+	desc: 			undefined
 }
 
 function init(){
@@ -35,6 +36,7 @@ function init(){
 	const pokemans = state.pokemans = new Map();
 	addPokemans(pokemans);
 
+	//assign all of the needed dom elements to the state for later use
 	state.image 	= document.getElementById("image");
 	state.id 		= document.getElementById("idfield");
 	state.name 		= document.getElementById("namefield");
@@ -63,16 +65,22 @@ function addPokemans(map){
 	let bulbasaur, charmander, squirtle, pikachu;
 
 	//TODO: future change to store all pokemon details in external JSON file would be nice
-	bulbasaur = new Pokemon("001", "Bulbasaur", "assets/images/Bulbasaur.png", ["Grass", "Poison"], 0.7, 6.9, 
+	bulbasaur = new Pokemon(
+		"001", "Bulbasaur", "assets/images/Bulbasaur.png", 
+		["Grass", "Poison"], 0.7, 6.9, 
 		"Bulbasaur is a small, quadruped Pokémon that has blue-green skin with darker patches.");
 
-	charmander = new Pokemon("004", "Charmander", "assets/images/Charmander.png", ["Fire"], 0.6, 8.5, 
+	charmander = new Pokemon(
+		"004", "Charmander", "assets/images/Charmander.png", 
+		["Fire"], 0.6, 8.5, 
 		"Charmander is a bipedal, reptilian Pokémon with a primarily orange body and blue eyes.");
 
-	squirtle = new Pokemon("007", "Squirtle", "assets/images/Squirtle.png", ["Water"], 0.5, 9.0, 
+	squirtle = new Pokemon("007", "Squirtle", "assets/images/Squirtle.png", 
+		["Water"], 0.5, 9.0, 
 		"Squirtle is a small Pokémon that resembles a light blue turtle.");
 
-	pikachu = new Pokemon("025", "Pikachu", "assets/images/Pikachu.png", ["Electric"], 0.4, 6.0, 
+	pikachu = new Pokemon("025", "Pikachu", "assets/images/Pikachu.png", 
+		["Electric"], 0.4, 6.0, 
 		"Pikachu is a short, chubby rodent Pokémon.");
 
 	//using the pokemon ID as the key for the map, could be name instead (it might be more readable)
@@ -88,21 +96,17 @@ function updateCurrDetails(event, key){
 
 	let pokemonEntry = undefined;
 
-	console.log(state.pokemans);
-
 	try {
 		pokemonEntry 			= state.pokemans.get(key);
-		console.log(pokemonEntry);
 		state.image.src 		= pokemonEntry.image;
 		state.id.innerText 		= pokemonEntry.id;
 		state.name.innerText 	= pokemonEntry.name;
-		//TODO: Support multi-types
 		state.type.innerText 	= pokemonEntry.type;
 		state.weight.innerText 	= pokemonEntry.weight;
 		state.height.innerText 	= pokemonEntry.height;
 		//TODO: Sort the description prefix for the paragraph, don't like duping
 		state.desc.innerText = "Description: " + pokemonEntry.description;
 	} catch(e) {
-		console.log("Must be using an invalid key? " + e);
+		console.log(e);
 	}
 }
